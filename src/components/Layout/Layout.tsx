f3c9c033f103eb6cdbed37bbc3fef6f1f281d4b9
Alexander Letnikow <reclamme@gmail.com>
2018-05-21 18:37:08 +0300

    Add simple and styled inputs to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
+import {Input} from "./Input";
+import {StyledInput} from "./StyledInput";
 
 export class Layout extends React.Component {
     public render(): React.ReactNode {
         return (
             <div>
                 hello world
+                <section>
+                    <h2>Simple Input</h2>
+                    <Input/>
+                </section>
+                <section>
+                    <h2>Styled Input</h2>
+                    <StyledInput/>
+                </section>
             </div>
         );
     }
